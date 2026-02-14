08d234f6649c1940faad800b5607d2c8de0afffd
Vladislav Nasevich <nasevich.vlasd.03@gmail.com>
2026-02-14 22:56:07 +0300

    vault backup: 2026-02-14 22:56:07

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -6,23 +6,20 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
-  const logoPath = joinSegments(baseDir, "static/re-logo.png")
+  const logoPath = joinSegments(baseDir, "static/main-logo.png")
   return (
     <div class={classNames(displayClass, "page-title")}>
-      <h2>
-        <a href={baseDir}>
-          <img style="aspect-ratio: 1.56/1; width: 100%" src={logoPath} placeholder={title}/>
-        </a>
-      </h2>
+      <a href={baseDir}>
+        <img style="aspect-ratio: 1.56/1; width: 100%" src={logoPath} placeholder={title}/>
+      </a>
     </div>
   )
 }
 
 PageTitle.css = `
 .page-title {
-  font-size: 1.75rem;
   margin: 0;
-  font-family: var(--titleFont);
+  padding-block: 0;
 }
 `
 
