d08b772a1da51e4b0cfb463e562bc88d0207eef9
Vladislav Nasevich <nasevich.vlasd.03@gmail.com>
2026-02-14 22:28:08 +0300

    vault backup: 2026-02-14 22:28:08

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { pathToRoot } from "../util/path"
+import { pathToRoot, joinSegments } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
@@ -6,9 +6,10 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  const logoPath = joinSegments(baseDir, "static/re-logo.png")
   return (
     <div class={classNames(displayClass, "page-title")}>
-      <img style="aspect-ratio: 1/1; width: 100px" src="../static/re-logo.png"/>
+      <img style="aspect-ratio: 1/1; width: 30px" src={logoPath}/>
       <h2><a href={baseDir}>{title}</a></h2>
     </div>
   )
